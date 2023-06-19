cde0c21ff68562d7a7b0c66a83348773b072f194
Dolapo Olaoluwa <dolliipissulelectrical16@gmail.com>
2023-06-19 08:12:04 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	int n = 0;
+	int m;
+
+	for (; n < 99; n++)
+	{
+		for (m = n + 1; m <= 99; m++)
+		{
+			putchar((n / 10) + '0');
+			putchar((n % 10) + '0');
+			putchar(' ');
+			putchar((m / 10) + '0');
+			putchar((m % 10) + '0');
+			if (n == 98 && m  == 99)
+			{
+				continue;
+			}
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}

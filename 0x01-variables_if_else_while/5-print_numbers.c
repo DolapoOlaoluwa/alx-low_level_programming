edf4a84ae9c9786d040200ed570dbbf7e2af017f
Dolapo Olaoluwa <dolliipissulelectrical16@gmail.com>
2023-06-19 04:55:49 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0 always success
+ */
+
+int main(void)
+{
+	int digit = 0;
+
+	for (; digit < 10; digit++)
+	{
+		printf("%d", digit);
+	}
+	printf("\n");
+	return (0);
+}

85e16599104cabb5f211f25fb9af6bfbd77c94c4
Dolapo Olaoluwa <dollipissulelectrical16@gmail.com>
2023-06-24 13:57:23 +0100

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints most numbers
+ * Return: nothing
+ */
+
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	for (; i <= 9; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar(i + '0');
+		}
+	}
+
+	_putchar('\n');
+}

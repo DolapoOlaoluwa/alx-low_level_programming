d1ff8b353656252b5e90ee2c6d7f3b6b1d7832c7
Dolapo Olaoluwa <dollipissulelectrical16@gmail.com>
2023-06-29 12:17:03 +0100

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two string.
+ * @dest: string to append to.
+ * @src: string to append.
+ * @n: number of byte of @src to append.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++, j++;
+	}
+	
+	dest[i] = '\0';
+
+	return (dest);
+}

56437777a291a1bb066505211da5a12520b1fb29
Dolapo Olaoluwa <dollipissulelectrical16@gmail.com>
2023-07-07 05:29:59 +0100

    tasks on recursion

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+void _puts_recursion(char *s);
+int _putchar(char c);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int sqrt_helper_recursion(int num, int root);
+int is_prime_number(int n);
+int prime_number_helper(int num, int factor);
+int palindromeChecker(char *str, int len, int i);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+
+#endif /*MAIN_H*/

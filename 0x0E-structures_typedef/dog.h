113dd36b28bb1cd0f34fa8e45b674b9189b20428
Dolapo Olaoluwa <dollipissulelectrical16@gmail.com>
2023-07-19 04:37:02 +0100

    all task on structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef _DOG_H
+#define _DOG_H
+#include <stdio.h>
+
+/**
+ * struct dog- structure for a dog.
+ * @name: name of Dog.
+ * @age: dog age.
+ * @owner: dog owner.
+ * Description: This struct gives us information of a dog.
+ */
+struct dog{
+	char *name;
+	float *age;
+	char *owner;
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif

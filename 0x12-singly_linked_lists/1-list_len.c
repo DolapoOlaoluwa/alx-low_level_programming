af21baf3d3ac47336dbc81d6c173de75d5ec41a6
Dolapo Olaoluwa <dollipissulelectrical16@gmail.com>
2023-07-29 23:00:51 +0100

    task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * list_len - finds the number of elements in a linked list
+ *@h: pointer to the the first node.
+ *Return: the number of elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+	list_t ptr;
+
+	ptr = h;
+
+	while (ptr != NULL)
+	{
+		count++;
+		ptr = ptr->next;
+	}
+
+	return (count);
+}

222f2f1c551f041f48df593dd3c1f5426237bf5b
Dolapo Olaoluwa <dollipissulelectrical16@gmail.com>
2023-07-30 14:19:17 +0100

    executer before the main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * work_b4_main - prints before main funtion is executed
+ * Return: Nothing
+ */
+
+void work_b4_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}

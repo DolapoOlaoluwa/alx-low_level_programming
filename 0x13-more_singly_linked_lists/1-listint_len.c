335ce39cf30c4ba6bb3dbacf3b1b422703d77d6d
Dolapo Olaoluwa <dollipissulelectrical16@gmail.com>
2023-07-31 19:59:30 +0100

    1-listint_len.c- n that returns the number of elements in a linked listint_t list.

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of
+ * 		elements in a linked listint_t list.
+ * @h: head pointer to the linked list
+ * Return: the number of elements.
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *ptr;
+	size_t number = 0;
+
+	ptr = malloc(sizeof(listint_t));
+	ptr = h;
+
+	while (ptr != NULL)
+	{
+		ptr = ptr->next;
+		number++;
+	}
+
+	return (number);
+}

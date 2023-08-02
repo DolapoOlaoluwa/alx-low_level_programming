a90cf0899b22a0aa0296420254b2c5792a0d169b
Dolapo Olaoluwa <dollipissulelectrical16@gmail.com>
2023-08-02 12:53:47 +0100

    free a list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -9,12 +9,12 @@
  */
 void free_listint(listint_t *head)
 {
-	listint_t *next;
+	listint_t *ptr;
 
-	while (head != NULL)
+	while (head)
 	{
-		next = head->next;
+		ptr = head->next;
 		free(head);
-		head = next;
+		head = ptr;
 	}
 }
---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list.
+ * @head: a pointer to head pointer.
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr;
+
+	if (head == NULL)
+		return;
+
+	while (*head != NULL)
+	{
+		ptr = (*head)->next;
+		free(*head);
+		*head = ptr;
+	}
+}

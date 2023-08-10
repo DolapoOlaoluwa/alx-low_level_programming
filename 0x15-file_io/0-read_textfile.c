bac357be6978b65f8ff9cde8aee6f41e04fc187f
Dolapo Olaoluwa <dollipissulelectrical16@gmail.com>
2023-08-10 23:32:12 +0100

    task 0, a function that reads the contents of a text file and prints it to the POSIX standard output

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads the contents of a text file and prints it to the
+ * POSIX standard output.
+ * @filename: name of file
+ * @letters: maximum number of characters to read and print
+ * Return: number of bytes of characters successfully read and printed
+ */
+
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	long int fd, read_bytes, written_bytes;
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * letters);
+	if (!buffer || !filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+	{
+		close(fd);
+		free(buffer);
+		return (0);
+	}
+
+	read_bytes = read(fd, buffer, letters);
+	if (r_bytes < 0)
+	{
+		close(fd);
+		free(buffer);
+		return (0);
+	}
+
+	written_bytes = write(STDOUT_FILENO, buffer, read_bytes);
+	if (written_bytes < read_bytes)
+	{
+		close(fd);
+		free(buffer);
+		return (0);
+	}
+	close(fd);
+	free(buffer);
+	return (written_bytes);
+}
